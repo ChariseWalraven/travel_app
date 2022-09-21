38b60c411c055a292902a9b1b87c8eef78aa37bc
Charise Walraven <cherri.walraven@gmail.com>
2022-09-21 22:55:26 +0200

    mess around with animations

--- lib/ui/screens/test.dart
+++ lib/ui/screens/test.dart
@@ -1,1 +1,69 @@
 
+import 'package:flutter/material.dart';
+import 'package:travel_app/ui/widgets.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TravelAppScaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            TestAnimatedContainer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TestAnimatedContainer extends StatefulWidget {
+  const TestAnimatedContainer({super.key});
+
+  @override
+  State<TestAnimatedContainer> createState() => _TestAnimatedContainerState();
+}
+
+class _TestAnimatedContainerState extends State<TestAnimatedContainer> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: AnimatedContainer(
+        width: selected ? 200.0 : 100.0,
+        height: selected ? 100.0 : 200.0,
+        alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
+        duration: const Duration(seconds: 2),
+        curve: Curves.linear,
+        decoration: BoxDecoration(
+          color: selected ? Colors.red : Colors.blue,
+          boxShadow: selected
+              ? [
+                  const BoxShadow(
+                    blurRadius: 0,
+                    offset: Offset(-20, -20),
+                  )
+                ]
+              : [
+                  const BoxShadow(
+                    blurRadius: 0,
+                    offset: Offset(20, 20),
+                  ),
+                ],
+        ),
+        child: const FlutterLogo(size: 75),
+      ),
+    );
+  }
+}
