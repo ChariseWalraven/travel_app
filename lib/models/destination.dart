03f311297f6bc618de1de4d972e8de4f40f118d6
Charise Walraven <cherri.walraven@gmail.com>
2022-09-24 13:54:11 +0200

    updates destination model to match data.json

--- lib/models/destination.dart
+++ lib/models/destination.dart
@@ -1,9 +1,30 @@
+import 'package:travel_app/models/attraction.dart';
+
 class Destination {
   final String name;
   final String imageAsset;
+  final String description;
+  final List<Attraction> attractions;
 
-  const Destination({
-    required this.name,
-    this.imageAsset = "assets/beach_square.png",
-  });
+  const Destination(
+      {required this.name,
+      this.imageAsset = "assets/beach_square.png",
+      required this.description,
+      required this.attractions});
+
+  factory Destination.fromJSON(Map<String, dynamic> json) {
+    List<Attraction> attractions = [];
+
+    if (json['attractions'] != null) {
+      json['attractions'].forEach((v) {
+        attractions.add(Attraction.fromJSON(v));
+      });
+    }
+    return Destination(
+      name: json['name'],
+      imageAsset: json['imageAsset'],
+      description: json['description'],
+      attractions: attractions,
+    );
+  }
 }
