d49857d96d4aeec36be05ccb1664ef508e25205e
Charise Walraven <cherri.walraven@gmail.com>
2022-09-24 13:56:12 +0200

    adds code to parse json to destinations

--- lib/data/destinations_helper.dart
+++ lib/data/destinations_helper.dart
@@ -1,1 +1,13 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:travel_app/models/destination.dart';
 
+class DestinationsHelper {
+  static Future<List<Destination>> getDestinations() async {
+    // bug: this finished after the age loads, so we don't get the destinations when the ui loads.
+    // I think the solution might be to use a future builder or something. Better ask in the slack group
+    String destinationsJSON = await rootBundle.loadString('assets/data.json');
+    List<dynamic> destinationsJSONDecoded = jsonDecode(destinationsJSON);
+    return destinationsJSONDecoded.map((e) => Destination.fromJSON(e)).toList();
+  }
+}
